b99ce397e05e91235c37f6701e180fe4d394275d
Robespra <rpages07@gmail.com>
2018-03-02 19:58:05 +0100

    updated folder

--- js/dialog.js
+++ js/dialog.js
@@ -16,11 +16,24 @@ $(".btn_cta_hero").click(function(e) {
    $("#fiction").removeClass("no-display");
    $("#fiction").addClass("display");
     $(".progress-bar-red").addClass("progress-bar-red-75");
+    $(".rap_casquette").addClass("casquette_active");
+    $(".rap_mic").addClass("rap_mic_active");
+    $(".seventy").addClass("percent_nodisplay");
+    $(".seventyfive").removeClass("percent_nodisplay");
+     $(".seventyfive").addClass("percent_display");
+    
     setTimeout(function() {
        $(".cui_loader").removeClass("display_loader");
        $(".cui_loader").addClass("remove_loader");
        $(".svg-logo").removeClass("svg-happyfreshlogo_thinking");
    }, 1800);
+    });
+
+    $(".triangle").click(function(e) {
+    $(".loader-container").addClass("loader-container_active");
+    $(".rap_vinyl").addClass("rap_vinyl_active");
+    $(".rec").addClass("rec_active");
+    $(".triangle").addClass("triangle_inactive");
     });
 
 $(".cui_first_button1").click(function(e) {
@@ -39,35 +52,46 @@ $(".cui_first_button1").click(function(e) {
     $(".svg-logo").addClass("svg-happyfreshlogo_thinking");
        $("#fiction").removeClass("display");
    $("#fiction").addClass("no-display");
-         $("#music").removeClass("no-display");
-   $("#music").addClass("display");
+         $("#music_combs").removeClass("no-display");
+   $("#music_combs").addClass("display");
    $(".progress-bar-red").removeClass("progress-bar-red-75");
    $(".progress-bar-red").addClass("progress-bar-red-80");
+   $(".rap_casquette").removeClass("casquette_active");
+   $(".rap_casquette").addClass("casquette_inactive");
+   $(".seventyfive").addClass("percent_nodisplay");
+   $(".eighty").removeClass("percent_nodisplay");
+     $(".eighty").addClass("percent_display");
+   setTimeout(function() {
+       $(".rap_casquette").addClass("casquette_inactive_nodisplay");
+   }, 1000);
+
     setTimeout(function() {
        $(".cui_loader").removeClass("display_loader");
        $(".cui_loader").addClass("remove_loader");
        $(".svg-logo").removeClass("svg-happyfreshlogo_thinking");
+       $(".rap_casquette").addClass("casquette_inactive_nodisplay");
    }, 2000);
     });
 
 
-    $(".cui_first_button2").click(function(e) {
-    $(".cui_after_cta").removeClass("cui_dialog_display");
-    $(".cui_after_cta").addClass("cui_dialog_hide");
-    $(".cui_first_non").removeClass("cui_dialog_hide");
-    $(".cui_first_non").addClass("cui_dialog_display");
-    $(".cui_first_non_message").addClass("bubble_displayed");
-    $(".cui_loader").removeClass("remove_loader");
-    $(".cui_loader").addClass("display_loader");
-    $(".cui_dialog_buttons_first_non").removeClass("cui_dialog_buttons_hidden");
-    $(".cui_dialog_buttons_first_non").addClass("cui_dialog_buttons_displayed");
-    $(".svg-logo").addClass("svg-happyfreshlogo_thinking");
-    setTimeout(function() {
-       $(".cui_loader").removeClass("display_loader");
-       $(".cui_loader").addClass("remove_loader");
-       $(".svg-logo").removeClass("svg-happyfreshlogo_thinking");
-   }, 2000);
+    $(".triangle_comb1").click(function(e) {
+    $(".box1").addClass("box_active");
     });
+
+    $(".triangle_comb2").click(function(e) {
+    $(".box2").addClass("box_active");
+    });
+
+    $(".triangle_comb3").click(function(e) {
+    $(".box3").addClass("box_active");
+    });
+
+    $(".triangle_comb4").click(function(e) {
+    $(".box4").addClass("box_active");
+    });
+
+
+
 
     $(".cui_first_oui_button2").click(function(e) {
     $(".cui_first_oui").removeClass("cui_dialog_display");
